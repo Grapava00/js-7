dec68f6da825a68ffce333a6a5c4e54bb020e35d
Grapava00 <85571784+Grapava00@users.noreply.github.com>
2021-11-11 17:10:59 +0400

    tasks

--- app.js
+++ app.js
@@ -1,29 +1,73 @@
-const formValidator = (form, fieldsConfig) => {
-    const validateField = () => {
+const form = document.querySelector("#user_registration_form");
+const firstName = document.querySelector("#first_name");
+const lastName = document.querySelector("#last_name");
+const zipCode = document.querySelector("#zip_code");
+const personalNumber = document.querySelector("#personal_number");
+const mobileNumber = document.querySelector("#mobile_number");
+const errorElement = document.querySelector("#error");
 
-    }
-    const validateOnchange = () => {
-        fieldsConfig.forEach((field) => {
-            const fieldElement = form.querySelector(`[name="${field.name}"]`);
-            fieldElement.addEventListener("input", e => {
-                console.log("input value", e.target.value);
-            });
-        });
-    }
 
-    const validateOnSubmit = () => {
-        form.addEventListener("submit", e => {
-            e.preventDefault();
-            console.log("submit")
-        })
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInputs();
+});
+
+function checkInputs() {
+  const firstNameValue = firstName.value.trim();
+  const lastNameValue = lastName.value.trim();
+
+  if (firstNameValue === "") {
+    setErrorFor(firstName, "required");
+  } else {
+    setSuccessFor(firstName);
+  }
+
+  if (lastNameValue === "") {
+    setErrorFor(lastName, "required");
+  } else {
+    setSuccessFor(lastName);
+  }
+
+  if (zipCode.value === "") {
+    setErrorFor(zipCode, "required");
+  } else {
+    setSuccessFor(zipCode);
+  }
+
+  if (personalNumber.value.length === 11) {
+    setSuccessFor(personalNumber);
+  } else {
+    setErrorFor(personalNumber, "max 11 digit");
+  }
+
+  if (mobileNumber.value === "") {
+    setErrorFor(mobileNumber, "required");
+  } else {
+    setSuccessFor(mobileNumber);
+  }
+
+  if (mobileNumber.value.length == 9) {
+    setSuccessFor(mobileNumber);
+  } else if (mobileNumber.value.length == 13) {
+    let firstCharacter = mobileNumber.value.slice(0, 1);
+    if (firstCharacter === "+") {
+      setSuccessFor(mobileNumber);
     }
-    validateOnSubmit();
-    validateOnchange();
+  } else {
+    setErrorFor(mobileNumber, "must 9 or 13 digit");
+  }
 }
 
-const fieldsConfig = [
-    { name: "first_name", rules: [{ required: true, message: "first name is required" }] }
-];
-const form = document.querySelector("#user_registration_form");
 
-formValidator(form, fieldsConfig);
+function setErrorFor(input, message) {
+  const formGroup = input.parentElement;
+  const span= formGroup.querySelector("#span");
+
+  span.innerText = message;
+  formGroup.className = "form_group error";
+}
+
+function setSuccessFor(input) {
+  const formGroup = input.parentElement;
+  formGroup.className = "form_group success";
+}
