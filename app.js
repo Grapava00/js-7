3ff2b941cedf67f1324729a5df895c0ac51d5db1
Grapava00 <85571784+Grapava00@users.noreply.github.com>
2021-11-11 17:40:12 +0400

    update style

--- app.js
+++ app.js
@@ -8,63 +8,63 @@ const mobileNumber = document.querySelector("#mobile_number");
 
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  checkInputs();
+    e.preventDefault();
+    checkInputs();
 });
 
 function checkInputs() {
-  if (firstName.value === "") {
-    setErrorFor(firstName, "required");
-  } else {
-    setSuccessFor(firstName);
-  }
-
-  if (lastName.value === "") {
-    setErrorFor(lastName, "required");
-  } else {
-    setSuccessFor(lastName);
-  }
-
-  if (zipCode.value === "") {
-    setErrorFor(zipCode, "required");
-  } else {
-    setSuccessFor(zipCode);
-  }
-
-  if (personalNumber.value.length === 11) {
-    setSuccessFor(personalNumber);
-  } else {
-    setErrorFor(personalNumber, "max 11 digit");
-  }
-
-  if (mobileNumber.value === "") {
-    setErrorFor(mobileNumber, "required");
-  } else {
-    setSuccessFor(mobileNumber);
-  }
-
-  if (mobileNumber.value.length == 9) {
-    setSuccessFor(mobileNumber);
-  } else if (mobileNumber.value.length == 13) {
-    let firstCharacter = mobileNumber.value.slice(0, 1);
-    if (firstCharacter === "+") {
-      setSuccessFor(mobileNumber);
+    if (firstName.value === "") {
+        setErrorFor(firstName, "required");
+    } else {
+        setSuccessFor(firstName);
     }
-  } else {
-    setErrorFor(mobileNumber, "must 9 or 13 digit");
-  }
+
+    if (lastName.value === "") {
+        setErrorFor(lastName, "required");
+    } else {
+        setSuccessFor(lastName);
+    }
+
+    if (zipCode.value === "") {
+        setErrorFor(zipCode, "required");
+    } else {
+        setSuccessFor(zipCode);
+    }
+
+    if (personalNumber.value.length === 11) {
+        setSuccessFor(personalNumber);
+    } else {
+        setErrorFor(personalNumber, "max 11 digit");
+    }
+
+    if (mobileNumber.value === "") {
+        setErrorFor(mobileNumber, "required");
+    } else {
+        setSuccessFor(mobileNumber);
+    }
+
+    if (mobileNumber.value.length == 9) {
+        setSuccessFor(mobileNumber);
+    } else if (mobileNumber.value.length == 13) {
+        let firstCharacter = mobileNumber.value.slice(0, 1);
+        if (firstCharacter === "+") {
+            setSuccessFor(mobileNumber);
+        }
+    } else {
+        setErrorFor(mobileNumber, "must 9 or 13 digit");
+    }
 }
 
 
 function setErrorFor(input, message) {
-  const formGroup = input.parentElement;
-  const span= formGroup.querySelector("#span");
+    const formGroup = input.parentElement;
+    const span = formGroup.querySelector("#span");
 
-  span.innerText = message;
-  formGroup.className = "form_group error";
+    span.innerText = message;
+    formGroup.className = "form_group error";
 }
 
 function setSuccessFor(input) {
-  const formGroup = input.parentElement;
-  formGroup.className = "form_group success";
+    const formGroup = input.parentElement;
+    formGroup.className = "form_group success";
 }
